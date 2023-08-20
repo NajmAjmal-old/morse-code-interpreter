bbcebc79838f6f107eea7dfb1d76a2251913fdf6
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-08-20 08:11:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -37,39 +37,38 @@ function initTranslation() {
         englishOutput.textContent = englishTranslation;
     }
 
-    function translateLive() {
-        const input = inputText.value.trim();
-        let morseTranslation = "";
-        let englishTranslation = "";
+function translateLive() {
+    const input = inputText.value.trim();
+    let morseTranslation = "";
+    let englishTranslation = "";
 
-        if (input !== "") {
-            if (input.match(/^[.-/\s]+$/)) {
-                const morseWords = input.split(" / ");
-                for (let morseWord of morseWords) {
-                    const morseChars = morseWord.split(" ");
-                    for (let morseChar of morseChars) {
-                        for (let key in morseCodeMap) {
-                            if (morseCodeMap[key] === morseChar) {
-                                englishTranslation += key;
-                                break;
-                            }
+    if (input !== "") {
+        if (input.match(/^[.\-/\/\s]+$/)) {
+            const morseWords = input.split(" / ");
+            for (let morseWord of morseWords) {
+                const morseChars = morseWord.split(" ");
+                for (let morseChar of morseChars) {
+                    for (let key in morseCodeMap) {
+                        if (morseCodeMap[key] === morseChar) {
+                            englishTranslation += key;
+                            break;
                         }
                     }
-                    englishTranslation += " ";
                 }
-            } else {
-                const englishChars = input.split(" ");
-                for (let char of englishChars) {
-                    if (char in morseCodeMap) {
-                        morseTranslation += morseCodeMap[char] + " ";
-                    } else if (char === "/") {
-                        morseTranslation += "/ ";
-                    }
+                englishTranslation += " ";
+            }
+        } else {
+            const englishChars = input.split(" ");
+            for (let char of englishChars) {
+                if (char in morseCodeMap) {
+                    morseTranslation += morseCodeMap[char] + " ";
+                } else if (char === "/") {
+                    morseTranslation += "/ ";
                 }
             }
         }
-
-        morseOutput.textContent = morseTranslation;
-        englishOutput.textContent = englishTranslation;
     }
+
+    morseOutput.textContent = morseTranslation;
+    englishOutput.textContent = englishTranslation;
 }
