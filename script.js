91efefa5d607144517d50224110faaeea175f521
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-08-20 08:08:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,75 @@
-const translateButton = document.getElementById("translateButton");
-const inputText = document.getElementById("inputText");
-const morseOutput = document.getElementById("morseOutput");
-const englishOutput = document.getElementById("englishOutput");
+let morseCodeMap;
 
-// Load Morse code map from JSON file
+// Fetch the Morse code map from morse.json
 fetch('morse.json')
     .then(response => response.json())
-    .then(morseCodeMap => {
-        translateButton.addEventListener("click", translateText);
+    .then(data => {
+        morseCodeMap = data;
+        initTranslation();
+    })
+    .catch(error => console.error('Error fetching morse.json:', error));
 
-        function translateText() {
-            const text = inputText.value.toUpperCase();
-            let morseTranslation = "";
-            let englishTranslation = "";
+function initTranslation() {
+    const translateButton = document.getElementById("translateButton");
+    const inputText = document.getElementById("inputText");
+    const morseOutput = document.getElementById("morseOutput");
+    const englishOutput = document.getElementById("englishOutput");
 
-            for (let char of text) {
-                if (char in morseCodeMap) {
-                    morseTranslation += morseCodeMap[char] + " ";
-                    englishTranslation += char + " ";
-                } else if (char === " ") {
-                    morseTranslation += "/ ";
-                    englishTranslation += "/ ";
+    translateButton.addEventListener("click", translateText);
+    inputText.addEventListener("input", translateLive);
+
+    function translateText() {
+        const text = inputText.value.toUpperCase();
+        let morseTranslation = "";
+        let englishTranslation = "";
+
+        for (let char of text) {
+            if (char in morseCodeMap) {
+                morseTranslation += morseCodeMap[char] + " ";
+                englishTranslation += char + " ";
+            } else if (char === " ") {
+                morseTranslation += "/ ";
+                englishTranslation += "/ ";
+            }
+        }
+
+        morseOutput.textContent = morseTranslation;
+        englishOutput.textContent = englishTranslation;
+    }
+
+    function translateLive() {
+        const input = inputText.value.trim();
+        let morseTranslation = "";
+        let englishTranslation = "";
+
+        if (input !== "") {
+            if (input.match(/^[.-/\s]+$/)) {
+                const morseWords = input.split(" / ");
+                for (let morseWord of morseWords) {
+                    const morseChars = morseWord.split(" ");
+                    for (let morseChar of morseChars) {
+                        for (let key in morseCodeMap) {
+                            if (morseCodeMap[key] === morseChar) {
+                                englishTranslation += key;
+                                break;
+                            }
+                        }
+                    }
+                    englishTranslation += " ";
+                }
+            } else {
+                const englishChars = input.split(" ");
+                for (let char of englishChars) {
+                    if (char in morseCodeMap) {
+                        morseTranslation += morseCodeMap[char] + " ";
+                    } else if (char === "/") {
+                        morseTranslation += "/ ";
+                    }
                 }
             }
-
-            morseOutput.textContent = morseTranslation;
-            englishOutput.textContent = englishTranslation;
         }
-    });
+
+        morseOutput.textContent = morseTranslation;
+        englishOutput.textContent = englishTranslation;
+    }
+}
